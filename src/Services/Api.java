d62c561e6d6929ae7f7da8347725cbb4b1a8af40
guilherme.bcatao <guilherme22catao@gmail.com>
2022-04-19 19:30:32 -0300

    Criando logica pra nao repetir um lugar que ja foi usado

--- src/Services/Api.java
+++ src/Services/Api.java
@@ -2,11 +2,11 @@ package Services;
 
 public class  Api{
 
+    String[] arr = country();
     public String[] data(int randomIndex) {
 
-        return country()[randomIndex].split(":");
 
-
+        return new String[0];
     }
 
     public String[] country() {
@@ -20,5 +20,25 @@ public class  Api{
                 "Catar:Doa", "Canadá:Otava", "Cazaquistão:Astana", "Chile:Santiago",
                 "China:Pequim", "Colômbia:Bogotá", "Coreia do Sul:Seul", "Costa Rica:São José"
         };
+    }
+
+    public String[] set (String[] arr, int randomIndex){
+
+        String[] setter = arr;
+        String Temp;
+        Boolean t = arr[randomIndex].split("://").equals(arr[randomIndex]);
+
+        if(!t){
+            System.out.println("Ja foi");
+        }
+        else {
+
+            Temp = setter[randomIndex] + "/" + randomIndex;
+            setter[0] = Temp;
+
+            setter = setter[randomIndex].split("://");
+
+            return setter;
+        }
     }
 }
